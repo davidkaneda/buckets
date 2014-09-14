5514521ab625b7bc09a920dd18783aa51927bbb3
David Kaneda <dk@morfunk.com>
2014-09-13 18:07:00 -0700

    Un-ignore the public dir for direct Heroku testing

---
+++ public/docs/api/api_project.js
@@ -0,0 +1,11 @@
+define({
+  "name": "buckets",
+  "version": "0.0.5",
+  "description": "Manage content better.",
+  "title": "Buckets REST API",
+  "url": "/api",
+  "generator": {
+    "version": "0.7.1",
+    "time": "2014-09-14T01:06:16.340Z"
+  }
+});
